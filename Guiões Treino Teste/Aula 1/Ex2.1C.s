06321fcc55d69b2d76922acb4ea22bec776b12d4
TomasWP <tomasfonseca14@ua.pt>
2024-04-06 14:28:46 +0100

    Update Ex2.1C.s

--- Guiões Treino Teste/Aula 1/Ex2.1C.s
+++ Guiões Treino Teste/Aula 1/Ex2.1C.s
@@ -11,9 +11,51 @@ str6:	.asciiz "\nValor em base 10 (unsigned), formatado: "
 	.data
 	.text
 	.globl main
-main:
-while:
+main:					# int main(void){
+while:					#	while(1){
 	li	$v0, printStr
 	la	$a0, str1
-	syscall
-
+	syscall				#		printStr(str1);
+	li	$v0, readInt10
+	syscall			
+	move	$t0, $v0		#		value = readInt10();
+	li	$v0, printStr
+	la	$a0, str2		
+	syscall				#		printStr(str2);
+	li	$v0, printInt10
+	move	$a0, $t0
+	syscall				#		printInt10(value);
+	li	$v0, printStr
+	la	$a0, str3
+	syscall				#		printStr(str3);
+	li	$v0, printInt	
+	move	$a0, $t0
+	li	$a0, 2			
+	syscall				#		printInt(value, 2);
+	li	$v0, printStr
+	la	$a0, str4
+	syscall				#		printStr(str4);
+	li	$v0, printInt
+	move	$a0, $t0
+	li	$a1, 16
+	syscall				#		printInt(value, 16);
+	li	$v0, printStr
+	la	$a0, str5
+	syscall				#		printStr(str5);
+	li	$v0, printInt
+	move	$a0, $t0
+	li	$a1, 10			
+	syscall				#		printInt(value, 10);
+	li	$v0, printStr
+	la	$a0, str6
+	syscall				#		printStr(str6);
+	li	$v0, printInt
+	move	$a0, $t0
+	li	$t1, 5
+	sll	$t1, $t1, 16
+	ori	$t1, $t1, 10
+	move	$a1, $t1
+	syscall				#		printInt(value, 10 | 5 << 16);
+					#	}
+	li	$v0, 0			#	return 0;	
+	jr	$ra			# }
