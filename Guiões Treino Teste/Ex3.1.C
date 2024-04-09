e553172fdd91d645f0ab0aecd7b83e756680d1a4
TomasWP <tomasfonseca14@ua.pt>
2024-04-09 20:13:42 +0100

    Ficheiros Treino Teste

--- Guiões Treino Teste/Ex3.1.C
+++ Guiões Treino Teste/Ex3.1.C
@@ -2,10 +2,11 @@
 
 int main(void){
 	
+	unsigned char value;
 	while(1){
 	      TRISB = TRISB | 0x000F;			// Configurar RB0-RB3 como input
 	      TRISE = TRISE & 0xFFF0;			// Configurar RE0-RE3 como output
 	      value = PORTB & 0x000F;			// ler valores
-	      LATE = (LATE & 0x000F) | value;		// escrever valor
+	      LATE = (LATE & 0xFFF0) | value;		// escrever valor
 	}
 }
